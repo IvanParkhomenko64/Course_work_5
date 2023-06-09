32f198cb379142a21175f077dd60cbb2d6f33afd
ivan <ippeng54@gmail.com>
2023-06-09 21:31:42 +0400

    2 commit

--- hh.py
+++ hh.py
@@ -1,5 +1,6 @@
 from parsingerror import ParsingError
 import requests
+from typing import Any
 
 class HeadHunter:
     """Класс для получения данных о вакансиях с сайта HeadHunter"""
@@ -7,7 +8,6 @@ class HeadHunter:
         self.__header = {
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:50.0) Gecko/20100101 Firefox/50.0"}
         self.__params = {
-         #   "text": keyword,
             "page": 0,  # номер страницы
             "per_page": 100  # количество отображаемых записей на странице
         }
@@ -22,11 +22,59 @@ class HeadHunter:
             formatted_salary[1] = salary['to'] if salary['currency'].lower() == 'rur' else salary['to'] * 80
         return formatted_salary
 
+    def get_hh_data(self, employers_ids: list[str]) -> list[dict[str, Any]]:
+        """Получение данных о работодателях и их вакансиях."""
+        data = []
+        for employer_id in employers_ids:
+            employer_data = requests.get(f'https://api.hh.ru/employers/{employer_id}', headers=self.__header, params=self.__params).json()
+            vacancies_data = []
+            count_open_vacancies = int(employer_data['open_vacancies'])
+            if count_open_vacancies <= 100:
+                params = {
+                    'employer_id': employer_id,  # ID 2ГИС
+                    'page': 0,  # Номер страницы
+                    'per_page': 100  # Кол-во вакансий на 1 странице
+                }
+                response = requests.get('https://api.hh.ru/vacancies', headers=self.__header, params=params).json()
+                vacancies_data.extend(response['items'])
+            elif count_open_vacancies >= 2000:
+                for i in range(19):
+                    params = {
+                        'employer_id': employer_id,  # ID 2ГИС
+                        'page': i,  # Номер страницы
+                        'per_page': 100  # Кол-во вакансий на 1 странице
+                    }
+                    response = requests.get('https://api.hh.ru/vacancies', headers=self.__header, params=params).json()
+                    vacancies_data.extend(response['items'])
+            else:
+                for i in range(count_open_vacancies // 100):
+                    params = {
+                        'employer_id': employer_id,  # ID 2ГИС
+                        'page': i,  # Номер страницы
+                        'per_page': 100  # Кол-во вакансий на 1 странице
+                    }
+                    response = requests.get('https://api.hh.ru/vacancies', headers=self.__header, params=params).json()
+                    vacancies_data.extend(response['items'])
+            data.append({
+                'employer': employer_data,
+                'vacancies': vacancies_data
+            })
+        return data
+
     def get_request(self):
-        response = requests.get('https://api.hh.ru/employers/39305', headers=self.__header, params=self.__params)
+        response = requests.get('https://api.hh.ru/employers/80', headers=self.__header, params=self.__params)
         if response.status_code != 200:
             raise ParsingError
-        return response.json()['items']
+        return response.json()['open_vacancies']
+
+    def get_page(self, page):
+        params = {
+            'employer_id': 80,  # ID 2ГИС
+            'page': page,  # Номер страницы
+            'per_page': 100  # Кол-во вакансий на 1 странице
+        }
+        response = requests.get('https://api.hh.ru/vacancies', headers=self.__header, params=params)
+        return response.json()
 
     def get_vacancies(self, pages_count=1):
         while self.__params['page'] < pages_count:
--- main.py
+++ main.py
@@ -5,8 +5,12 @@ def main():
     vacancies_json = []
     #keyword = input('Введите ключевое слово для поиска: ')
     hh = HeadHunter()
-    hh.get_vacancies(pages_count=10)
-    print(hh.get_formatted_vacancies())
+    #hh.get_vacancies(pages_count=10)
+    #print(hh.get_request())
+    #print(hh.get_page(0))
+    employers_list = ['733', '67611']
+    n = hh.get_hh_data(employers_list)
+    print(n[1]['vacancies'])
 
 
 if __name__ == '__main__':
